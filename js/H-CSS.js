51cc949c13aa5b65d95cb0c98c85ed99745aa383
胡永定 <hyd19931224@live.com>
2017-10-26 17:50:31 +0800

    提示框

---
+++ js/H-CSS.js
@@ -0,0 +1,86 @@
+/*
+ * @Author: Fred.Hu
+ * @Date: 2017-10-26 09:30:22
+ * @Last Modified by: Fred.Hu
+ * @Last Modified time: 2017-10-26 10:09:26
+ */
+
+// 提示弹窗
+function noticeError(WaringMes) {
+    $("#notice").fadeOut();
+    $("#notice").remove();
+
+    $('body').append('' +
+        '<div id="notice" class="notice notice-error" hidden>' +
+        '<span>' + WaringMes + '</span>' +
+        '<i onclick="$(this).closest($(\'#notice\')).fadeOut(\'slow\');" class="fa fa-times" aria-hidden="true"></i>' +
+        '</div>');
+    $("#notice").fadeIn();
+
+    setTimeout(function() {
+        $("#notice").fadeOut('slow');
+    }, 5000);
+}
+
+function noticeSuccess(WaringMes) {
+    $("#notice").fadeOut();
+    $("#notice").remove();
+
+    $('body').append('' +
+        '<div id="notice" class="notice notice-success" hidden>' +
+        '<span>' + WaringMes + '</span>' +
+        '<i onclick="$(this).closest($(\'#notice\')).fadeOut(\'slow\');" class="fa fa-times" aria-hidden="true"></i>' +
+        '</div>');
+    $("#notice").fadeIn();
+
+    setTimeout(function() {
+        $("#notice").fadeOut('slow');
+    }, 5000);
+}
+
+function noticeWarning(WaringMes) {
+    $("#notice").fadeOut();
+    $("#notice").remove();
+
+    $('body').append('' +
+        '<div id="notice" class="notice notice-warning" hidden>' +
+        '<span>' + WaringMes + '</span>' +
+        '<i onclick="$(this).closest($(\'#notice\')).fadeOut(\'slow\');" class="fa fa-times" aria-hidden="true"></i>' +
+        '</div>');
+    $("#notice").fadeIn();
+
+    setTimeout(function() {
+        $("#notice").fadeOut('slow');
+    }, 5000);
+}
+
+function noticeSecondary(WaringMes) {
+    $("#notice").fadeOut();
+    $("#notice").remove();
+
+    $('body').append('' +
+        '<div id="notice" class="notice notice-secondary" hidden>' +
+        '<span>' + WaringMes + '</span>' +
+        '<i onclick="$(this).closest($(\'#notice\')).fadeOut(\'slow\');" class="fa fa-times" aria-hidden="true"></i>' +
+        '</div>');
+    $("#notice").fadeIn();
+
+    setTimeout(function() {
+        $("#notice").fadeOut('slow');
+    }, 5000);
+}
+
+function noticeAndroid(WaringMes) {
+    $("#notice").fadeOut();
+    $("#notice").remove();
+
+    $('body').append('' +
+        '<div id="notice" class="notice-Android" hidden>' +
+        '<div>' + WaringMes + '</div>' +
+        '</div>');
+    $("#notice").fadeIn();
+
+    /*setTimeout(function() {
+        $("#notice").fadeOut('slow');
+    }, 5000);*/
+}
